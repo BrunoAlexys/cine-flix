90395ea022c3e4582e5b913a141c8aabf2ac7084
Bruno Moura <124813743+BrunoAlexys@users.noreply.github.com>
2024-08-13 17:02:37 -0300

    Adicionando o icone no botão de ver mais

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -3,8 +3,20 @@ import styles from './Comments.module.css';
 import Send from '../../assets/send.png';
 import Boy from '../../assets/garoto.png';
 import Girl from '../../assets/menina.png';
+import { useState } from 'react';
+import { CommentsList } from '../../data/CommentsList';
+import SeeMore from '../../assets/ver-mais.png';
 
 export const Comments = () => {
+
+    const [visebleComments, setVisebleComments] = useState(3);
+
+    const fetchMoreData = () => {
+        setTimeout(() => {
+            setVisebleComments(prevVisibleComments => prevVisibleComments + 3);
+        }, 300)
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.title}>
@@ -23,21 +35,33 @@ export const Comments = () => {
                     <button className={styles.send}><img src={Send} alt="Botão de enviar" /></button>
                 </div>
             </div>
-            <div className={styles.comment}>
-                <div className={styles.containerComment}>
-                    <div className={styles.containerPerfil}>
-                        <div className={styles.perfilSm}>
-                            <img className={styles.perfilImg} src={Boy} alt="Imagem de perfil" />
-                        </div>
-                        <div className={styles.containerName}>
-                            <p className={styles.name}>José Marcos</p>
+            {
+                CommentsList.slice(0, visebleComments).map((comment, index) => (
+                    <div key={index} className={styles.comment}>
+                        <div className={styles.containerComment}>
+                            <div className={styles.containerPerfil}>
+                                <div className={styles.perfilSm}>
+                                    <img className={styles.perfilImg} src={comment.img || Boy} alt="Imagem de perfil" />
+                                </div>
+                                <div className={styles.containerName}>
+                                    <p className={styles.name}>{comment.name}</p>
+                                </div>
+                            </div>
+                            <div className={styles.containerMensage}>
+                                <p className={styles.mensage}>{comment.mensage}</p>
+                            </div>
                         </div>
                     </div>
-                    <div className={styles.containerMensage}>
-                        <p className={styles.mensage}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quam fugiat alias et. Tempore velit facere obcaecati quam quos porro molestias libero cupiditate quasi itaque, vel ipsum cumque excepturi ea!</p>
-                    </div>
+                ))}
+
+            {visebleComments < CommentsList.length && (
+                <div className={styles.loadMoreContainer}>
+                    <button onClick={fetchMoreData} className={styles.loadMoreButton}>
+                        <img src={SeeMore} alt="Icone do botão ver mais" width={30} height={30}/>
+                        Ver mais
+                    </button>
                 </div>
-            </div>
+            )}
         </div>
     );
 }
---
+++ src/data/CommentsList.ts
@@ -0,0 +1,43 @@
+type CommentType = {
+    name: string;
+    mensage: string;
+    img: string;
+}
+
+export const CommentsList: CommentType[] = [
+    {
+        name: 'João Victor',
+        mensage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quam fugiat alias et. Tempore velit facere obcaecati quam quos porro molestias libero cupiditate quasi itaque, vel ipsum cumque excepturi ea!',
+        img: 'https://mighty.tools/mockmind-api/content/cartoon/9.jpg'
+    },
+
+    {
+        name: 'José Marcos',
+        mensage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quam fugiat alias et. Tempore velit facere obcaecati quam quos porro molestias libero cupiditate quasi itaque, vel ipsum cumque excepturi ea!',
+        img: 'https://mighty.tools/mockmind-api/content/human/65.jpg'
+    },
+
+    {
+        name: 'Maria Eduarda',
+        mensage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quam fugiat alias et. Tempore velit facere obcaecati quam quos porro molestias libero cupiditate quasi itaque, vel ipsum cumque excepturi ea!',
+        img: 'https://mighty.tools/mockmind-api/content/human/44.jpg'
+    },
+
+    {
+        name: 'Fernando Silva',
+        mensage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quam fugiat alias et. Tempore velit facere obcaecati quam quos porro molestias libero cupiditate quasi itaque, vel ipsum cumque excepturi ea!',
+        img: 'https://mighty.tools/mockmind-api/content/human/5.jpg'
+    },
+
+    {
+        name: 'Ana Clara',
+        mensage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quam fugiat alias et. Tempore velit facere obcaecati quam quos porro molestias libero cupiditate quasi itaque, vel ipsum cumque excepturi ea!',
+        img: 'https://mighty.tools/mockmind-api/content/human/43.jpg'
+    },
+
+    {
+        name: 'Pedro Henrique',
+        mensage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quam fugiat alias et. Tempore velit facere obcaecati quam quos porro molestias libero cupiditate quasi itaque, vel ipsum cumque excepturi ea!',
+        img: 'https://mighty.tools/mockmind-api/content/human/42.jpg'
+    },
+]
